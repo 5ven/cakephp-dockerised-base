661b93cadfb1b62c44bc75e2d3df88980fc1b752
5ven <sven.kauber@gmail.com>
2024-03-24 22:13:22 +0000

    Improved documentation, renamed a script and fixed the setup script

--- helpers/fix-permissions.sh
+++ helpers/fix-permissions.sh
@@ -6,4 +6,4 @@ CURRENT_USER_ID=$(id -u)
 DOCKER_COMPOSE_CMD=$(find_docker_compose_command)
 
 $DOCKER_COMPOSE_CMD exec cakephp chown www-data:www-data /var/www/html
-$DOCKER_COMPOSE_CMD exec cakephp chown -R $CURRENT_USER_ID:www-data /var/www/html/.*
+$DOCKER_COMPOSE_CMD exec cakephp chown -R $CURRENT_USER_ID:www-data /var/www/html/*
